276216a92bdef799e4347d60d66096931b08a2f8
ambiesoft <ambiesoft.trueff@gmail.com>
2021-07-17 08:44:14 +0900

    copy lsPy

--- dist.bat
+++ dist.bat
@@ -1,4 +1,4 @@
 setlocal
 set TARGETDIR=C:\Linkout\checkserver\
 mkdir %TARGETDIR%
-C:\LegacyPrograms\FFC\FFC.exe "%~dp0checkserver.py" "%~dp0ok.wav" "%~dp0ng.wav" /to:"%TARGETDIR%"
+C:\LegacyPrograms\FFC\FFC.exe "%~dp0checkserver.py" "%~dp0ok.wav" "%~dp0ng.wav" "%~dp0lsPy\lspy.py" "%~dp0lsPy\logger.py" /to:"%TARGETDIR%"

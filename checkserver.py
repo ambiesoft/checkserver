550bf4c6d0b9d9ac4a5148f6e05d5ada3f85964f
ambiesoft <ambiesoft.trueff@gmail.com>
2021-07-17 00:08:35 +0900

    scheduled

--- checkserver.py
+++ checkserver.py
@@ -7,22 +7,23 @@ import tkinter as tk
 from tkinter import messagebox 
 import urllib.request
 import datetime
+import re
+import dns.resolver
 
 APPNAME = 'checkserver'
 
 root = tk.Tk() 
-root.withdraw() #小さなウィンドウを表示させない 
+# root.withdraw() #小さなウィンドウを表示させない 
 
 def checkdns():
     import socket
-    addr1 = socket.gethostbyname('ambiesoft.mooo.com') 
-    if addr1 != '192.168.3.99':
-        raise(NameError('not 192.168.3.99'))
+    addr1 = socket.gethostbyname('ambiesoft.com') 
+    if addr1 != '192.168.3.97':
+        raise(NameError('not 192.168.3.97'))
 
 def checkblog():
-    fp = urllib.request.urlopen("http://ambiesoft.mooo.com/blog/")
+    fp = urllib.request.urlopen("https://ambiesoft.com/blog/")
     mybytes = fp.read()
-
     mystr = mybytes.decode("utf8")
     fp.close()
 
@@ -30,14 +31,33 @@ def checkblog():
         raise(IOError('not blog'))
 
 def checkdb():
-    fp = urllib.request.urlopen("http://ambiesoft.mooo.com/boolog/dbcheck")
+    fp = urllib.request.urlopen("https://ambiesoft.com/boolog/dbcheck")
     mybytes = fp.read()
-
     mystr = mybytes.decode("utf8")
     fp.close()
 
     if -1 == mystr.find('dbcheck is ok'):
         raise(IOError('db is not ok'))
+
+def checkip():
+    fp = urllib.request.urlopen("http://checkip.dyndns.com/")
+    ipstr = fp.read().decode("utf8")
+    ip = re.findall( r'[0-9]+(?:\.[0-9]+){3}', ipstr )[0]
+    if not ip:
+        raise(IOError('current ip not found'))
+    
+    my_resolver = dns.resolver.Resolver()
+
+    # 8.8.8.8 is Google's public DNS server
+    my_resolver.nameservers = ['8.8.8.8']
+    # answers = my_resolver.query('ambiesoft.com', 'A')
+    answers = my_resolver.resolve('ambiesoft.com', 'A')
+    for answer in answers:
+        dnsip = answer.to_text()
+        break
+
+    if ip != dnsip:
+        raise(NameError('Current ip != dns ip ({} != {}'.format(ip,dnsip)))
 
 def playsoundcommon(ok):
     from playsound import playsound
@@ -58,17 +78,17 @@ def main():
 
     try:
         checkdns()
-        # messagebox.showinfo('title', 'message')
         checkblog()
         checkdb()
-
-        playoksound()
+        checkip()
+        # playoksound()
 
     except Exception as e:
         messagebox.showerror(APPNAME, repr(e)) #APPNAME, type(e).__name__ + (str)e)
-        playngsound()
+        # playngsound()
 
     logfile.close()
 
 if __name__ == '__main__':
+    messagebox.showerror(APPNAME, 'aaaaaaaaaaaaaaaaaaa')
     main()
